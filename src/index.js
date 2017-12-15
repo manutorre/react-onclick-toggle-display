5355adee07534ae80820c175b157a8cefb6cd33a
Manuel <manuel.torre@aero.tur.ar>
2017-12-14 21:09:27 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,121 @@
-import React, {Component} from 'react'
+import React from 'react'
+import Proptypes from 'prop-types'
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+export default class OnClickToggleDisplay extends React.Component{
+
+  /*This components is useful for showing some content whenever you want.
+   If only the required props are passed, the content will close if you click outside of It.
+    props ->
+      openerNode: is the node that onClick opens up the node passed by the content prop. It is required
+      preventFromCloseElements: It's an array of strings. each string represents an Id or a ClassName. If you click on an element with one of these strings
+        as id or class, the popover will not close (event if It is outside the popover)
+      preventInsideOfElements: works like preventFromCloseElements. The important difference is that if you click on an element that is INSIDE an element with
+        any of the strings passed as id or class, then It will not close (even if It is not outside the popover)
+      closeFromInsideElements: It's an array of strings. As It's name suggests, if you click on an element with class or id that is the same as one of the
+        strings passed, then the popover will hide
+      onOpening: expects a function that will be fired when content is shown
+  */
+
+
+	constructor(props){
+		super(props)
+		this.state = {
+			show:false
+		}
+    //this binding is for keeping the component context and calling refs, props, state...
+    this.checkClickLocation = this.checkClickLocation.bind(this)
+	}
+
+	toggleClickListener(){
+    if (!this.state.show){
+      document.addEventListener('click', this.checkClickLocation, false);
+    }
+    else{
+      document.removeEventListener('click', this.checkClickLocation, false);
+    }
+	}
+
+  toggleAeroPopover(){
+    if (this.props.onOpening && !this.state.show) {
+      this.props.onOpening();
+    }
+    this.toggleClickListener()
+    this.setState({
+      show: !this.state.show
+    })
   }
+
+  checkClickLocation(e){ //fires on every click when popover is open.
+    if (!this.checkCloseFromInsideElement(e.target) && //if user did not click in any closeFromInsideElements passed as prop
+       ((this.refs.popover && this.refs.popover.contains(e.target)) || //checks if is clicking inside of content
+         this.checkPreventCloseNodes(e.target) ||
+         this.checkPreventInsideOfNodes(e.target)))
+       {
+         return;
+       }
+    this.toggleAeroPopover();
+  }
+
+  checkPreventInsideOfNodes(target){ //you can pass as prop all the ids you want to check if you clicked inside
+    if (!this.props.preventInsideOfElements) {
+      return false
+    }
+    return this.props.preventInsideOfElements.some( (idOrClass) => {
+      const elementToCheck = document.getElementById(idOrClass) ? document.getElementById(idOrClass) : document.getElementsByClassName(idOrClass)[0]
+      return (elementToCheck && elementToCheck.contains(target))
+    })
+  }
+
+
+  checkPreventCloseNodes(target){ //you can pass as prop all the ids you want the popover prevent from closing
+    if (!target.id && !target.className || !this.props.preventFromCloseElements){
+      return false
+    }
+    else{
+      const targetElement = target.id ? target.id : target.className
+      return this.props.preventFromCloseElements.some((idOrClass) => {
+        return targetElement == idOrClass
+      })
+    }
+  }
+
+
+  checkCloseFromInsideElement(target){
+    if ((!target.id && !target.className) || !this.props.closeFromInsideElements){
+      return false
+    }
+    else{
+      const targetElement = target.id ? target.id : target.className
+      return this.props.closeFromInsideElements.some((idOrClass) => {
+        return targetElement == idOrClass
+      })
+    }
+  }
+
+
+  componentWillUnmount(){
+    document.removeEventListener('click', this.checkClickLocation, false)
+  }
+
+	render(){
+		const openerClass= this.state.show ? "open-content" : ""
+		return (
+			<div className={this.props.containerClass || ""}>
+				<div ref="popover">
+					{this.state.show && this.props.children}
+				</div>
+				<div ref="openerNode" onClick={() => this.toggleAeroPopover()} className={openerClass}>
+					{this.props.openerNode}
+				</div>
+			</div>
+		)
+	}
+
+}
+
+OnClickToggleDisplay.propTypes = {
+	openerNode:Proptypes.node.isRequired,
+	preventFromCloseElements: Proptypes.arrayOf(Proptypes.string),
+  preventInsideOfElements: Proptypes.arrayOf(Proptypes.string),
+  closeFromInsideElements: Proptypes.arrayOf(Proptypes.string),
 }
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,23 +1,203 @@
 import expect from 'expect'
 import React from 'react'
-import {render, unmountComponentAtNode} from 'react-dom'
+import ReactDOM from 'react-dom'
+import {renderToStaticMarkup as render} from 'react-dom/server'
+import { configure } from 'enzyme';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import Adapter from 'enzyme-adapter-react-16';
+import simulant from 'simulant'
 
-import Component from 'src/'
 
-describe('Component', () => {
-  let node
+import OnClickToggleDisplay from 'src/'
 
-  beforeEach(() => {
-    node = document.createElement('div')
-  })
+configure({ adapter: new Adapter() });
 
-  afterEach(() => {
-    unmountComponentAtNode(node)
-  })
-
-  it('displays a welcome message', () => {
-    render(<Component/>, node, () => {
-      expect(node.innerHTML).toContain('Welcome to React components')
-    })
-  })
+beforeEach(() => {
+  ReactDOM.unmountComponentAtNode(document.body) //avoid issues with keeping the component mounted
 })
+
+describe('Hiding content', () => {
+  it('does not render inner content by default', () => {
+    expect(render(<OnClickToggleDisplay openerNode={(<a>boton</a>)}><div>Test</div></OnClickToggleDisplay>)).toExclude('<div>Test</div>')
+  })
+
+  it('Hides content when openerNode is not provided', () => {
+    expect(render(<OnClickToggleDisplay><div>Test</div></OnClickToggleDisplay>)).toExclude('Test')
+  })
+
+
+  it('Hides inside content when clicking opener node when already showing content', () => {
+    const boton = <a className="opener">boton</a>
+    const content = <div>Test</div>
+    const outsideContent = shallow(<div>Outside content</div>)
+    const wrapper = shallow(
+      <OnClickToggleDisplay openerNode={boton}>
+        {content}
+      </OnClickToggleDisplay>);
+    wrapper.find('.opener').parent().simulate('click');
+    wrapper.find('.opener').parent().simulate('click');
+    expect(render(wrapper)).toExclude('Test');
+  });
+
+  it('Hides inside content when clicking somewhere outside of It', () => { //Enzyme's shallow rendering event handler's works with components "onClick" props... so could not use It here
+    ReactDOM.render(
+      (<div className="wrapper">
+        <div className="outside">Outside content</div>
+        <OnClickToggleDisplay openerNode={<a className="opener">boton</a>}>
+          <div className="content">Test</div>
+        </OnClickToggleDisplay>);
+      </div>), document.body)
+    simulant.fire(document.getElementsByClassName("opener")[0], 'click')
+    simulant.fire(document.getElementsByClassName("outside")[0], 'click')
+    expect(document.getElementsByClassName("content")[0]).toNotExist() ;
+  });
+
+  it('Hides inside content when clicking somewhere inside but with class passed as closeFromInsideElements', () => { //Enzyme's shallow rendering event handler's works with components "onClick" props... so could not use It here
+    ReactDOM.render(
+      (<div className="wrapper">
+        <div className="outside">Outside content</div>
+        <OnClickToggleDisplay openerNode={<a className="opener">boton</a>} closeFromInsideElements={["closer-button"]}>
+          <div className="content">
+            <a className="closer-button">button</a>
+            <span>Test</span>
+          </div>
+        </OnClickToggleDisplay>);
+      </div>), document.body)
+    simulant.fire(document.getElementsByClassName("opener")[0], 'click')
+    simulant.fire(document.getElementsByClassName("closer-button")[0], 'click') //clicks the element passed as closeFromInsideElements will remove the content
+    expect(document.getElementsByClassName("content")[0]).toNotExist() ;
+  });
+
+  it('Hides inside content when clicking somewhere inside but with an Id passed as closeFromInsideElements', () => { //Enzyme's shallow rendering event handler's works with components "onClick" props... so could not use It here
+    ReactDOM.render(
+      (<div className="wrapper">
+        <div className="outside">Outside content</div>
+        <OnClickToggleDisplay openerNode={<a className="opener">boton</a>} closeFromInsideElements={["closer-button"]}>
+          <div className="content">
+            <a id="closer-button">button</a>
+            <span>Test</span>
+          </div>
+        </OnClickToggleDisplay>);
+      </div>), document.body)
+    simulant.fire(document.getElementsByClassName("opener")[0], 'click')
+    simulant.fire(document.getElementById("closer-button"), 'click') //clicks the element passed as closeFromInsideElements will remove the content
+    expect(document.getElementsByClassName("content")[0]).toNotExist() ;
+  });
+
+  it('Clicking at outer nodes descendant from nodes with preventFromCloseElements classes will hide the content', () => { //Enzyme's shallow rendering event handler's works with components "onClick" props... so could not use It here
+    ReactDOM.render(
+      (<div className="wrapper">
+        <div className="outside">Outside content</div>
+        <OnClickToggleDisplay openerNode={<a className="opener">boton</a>} preventFromCloseElements={['wrapper']}>
+          <div className="content">Test</div>
+        </OnClickToggleDisplay>);
+      </div>), document.body)
+    simulant.fire(document.getElementsByClassName("opener")[0], 'click')
+    simulant.fire(document.getElementsByClassName("outside")[0], 'click') //checks clicking at outside and wrapper divs... that are not inside of shown content
+    expect(document.getElementsByClassName("content")[0]).toNotExist() ;
+  });
+})
+
+
+
+
+
+describe('Showing content', () => {
+  it('Shows inside content when opener node is clicked', () => {
+    const wrapper = shallow(
+      <OnClickToggleDisplay openerNode={<a className="opener">boton</a>}>
+        <div>Test</div>
+      </OnClickToggleDisplay>);
+      wrapper.find('.opener').parent().simulate('click');
+      expect(render(wrapper)).toInclude('Test')
+    });
+
+    it('Shows inside content when clicking somewhere inside of It', () => { //Enzyme's shallow rendering event handler's works with components "onClick" props... so could not use It here
+      ReactDOM.render(
+        (<div className="wrapper">
+          <div className="outside">Outside content</div>
+          <OnClickToggleDisplay openerNode={<a className="opener">boton</a>}>
+            <div className="content">Test</div>
+          </OnClickToggleDisplay>);
+        </div>), document.body)
+      simulant.fire(document.getElementsByClassName("opener")[0], 'click')
+      simulant.fire(document.getElementsByClassName("content")[0], 'click')
+      expect(document.getElementsByClassName("content")[0]).toExist() ;
+    });
+
+    it('Clicking at outer nodes with classes or ids passed as preventFromCloseElements wont hide the content', () => { //Enzyme's shallow rendering event handler's works with components "onClick" props... so could not use It here
+      ReactDOM.render(
+        (<div id="wrapper">
+          <div className="outside">Outside content</div>
+          <OnClickToggleDisplay openerNode={<a className="opener">boton</a>} preventFromCloseElements={['outside','wrapper']}>
+            <div className="content">Test</div>
+          </OnClickToggleDisplay>);
+        </div>), document.body)
+      simulant.fire(document.getElementsByClassName("opener")[0], 'click')
+      simulant.fire(document.getElementsByClassName("outside")[0], 'click') //checks clicking at outside and wrapper divs... that are not inside of shown content
+      simulant.fire(document.getElementById("wrapper"), 'click')
+      expect(document.getElementsByClassName("content")[0]).toExist() ;
+    });
+
+    it('Clicking at outer nodes inside of nodes with class passed as preventFromCloseElements wont hide the content', () => { //Enzyme's shallow rendering event handler's works with components "onClick" props... so could not use It here
+      ReactDOM.render(
+        (<div className="wrapper">
+          <div className="outside">Outside content</div>
+          <OnClickToggleDisplay openerNode={<a className="opener">boton</a>} preventInsideOfElements={['wrapper']}>
+            <div className="content">Test</div>
+          </OnClickToggleDisplay>);
+        </div>), document.body)
+      simulant.fire(document.getElementsByClassName("opener")[0], 'click')
+      simulant.fire(document.getElementsByClassName("outside")[0], 'click') //the node with outside class is descendant from a node with wrapper as class name
+      expect(document.getElementsByClassName("content")[0]).toExist() ;
+    });
+
+    it('Clicking at outer nodes inside of nodes with Id passed as preventFromCloseElements wont hide the content', () => { //Enzyme's shallow rendering event handler's works with components "onClick" props... so could not use It here
+      ReactDOM.render(
+        (<div id="wrapper">
+          <div className="outside">Outside content</div>
+          <OnClickToggleDisplay openerNode={<a className="opener">boton</a>} preventInsideOfElements={['wrapper']}>
+            <div className="content">Test</div>
+          </OnClickToggleDisplay>);
+        </div>), document.body)
+      simulant.fire(document.getElementsByClassName("opener")[0], 'click')
+      simulant.fire(document.getElementsByClassName("outside")[0], 'click') //the node with outside class is descendant from a node with wrapper as class name
+      expect(document.getElementsByClassName("content")[0]).toExist() ;
+    });
+
+
+    describe('on opening', () => {
+      it('fires onOpening function when content is shown', () => {
+        const logging = function(){
+          console.log("manu")
+        }
+        const spied = sinon.spy(logging)
+        const boton = <a className="opener">boton</a>
+        const content = <div>Test</div>
+        const wrapper = shallow(
+          <OnClickToggleDisplay openerNode={boton} onOpening={spied}>
+            {content}
+          </OnClickToggleDisplay>);
+        wrapper.find('.opener').parent().simulate('click');
+        sinon.assert.called(spied);
+      })
+      it('adds the open class to the opener node', () => {
+        const boton = <a className="opener">boton</a>
+        const content = <div>Test</div>
+        const wrapper = shallow(
+          <OnClickToggleDisplay openerNode={boton}>
+            {content}
+          </OnClickToggleDisplay>);
+        wrapper.find('.opener').parent().simulate('click');
+        expect(wrapper.find('.open-content').length).toEqual(1);
+      })
+
+    });
+
+
+
+
+
+
+});
